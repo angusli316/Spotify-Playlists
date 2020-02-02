79753c427229a36025b546712b42276e2de8b265
prestontai <prestonttt@yahoo.com>
2020-02-02 13:37:45 -0800

    spotify copier

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ def history():
 song_freq = dict()
 
 class Track:
-    def __init__(self, name, artist):
+    def __init__(self, name, artist, tid):
         self.name = name
         self.artist = artist
+        self.id = tid
     def __repr__(self):
         return self.name + ' - ' + self.artist + '\n'
     def __eq__(self, right):
@@ -35,13 +36,12 @@ class Playlist:
         self.tracks = set()
         try:
             for t in tracks['items']:
-                print(t['track']['id'])
-                track_object = t['track']['name'], t['track']['artists'][0]['name']
+                track_object = t['track']['name'], t['track']['artists'][0]['name'], t['track']['id']
                 if track_object in song_freq:
                     song_freq[track_object] += 1
                 else:
                     song_freq[track_object] = 1
-                self.tracks.add(Track(name=track_object[0], artist=track_object[1]))
+                self.tracks.add(Track(name=track_object[0], artist=track_object[1], tid=track_object[2]))
         except TypeError:
             pass
     def intersection(self, right):
@@ -100,17 +100,22 @@ def playlist_intersect():
         else:
             common = one.intersection(common)
 
-def playlist_destruct(p):
-     s_id = ['2bJvI42r8EF3wxjOuDav4r']
-     sp.user_playlist_replace_tracks(sp.me()['id'], p.id, s_id)
+def playlist_destruct(src, dest):
+     #s_id = ['2bJvI42r8EF3wxjOuDav4r']
+     id_list = []
+     for t in src.tracks:
+        if t.id is not None:
+            id_list.append(t.id)
+     sp.user_playlist_replace_tracks(sp.me()['id'], dest.id, id_list)
 
-     #image_path = 'swag.png'
-     #sp.playlist_upload_cover_image(p.id, image_path)
+     #image_path = 'swag.jpeg'
+     #csp.playlist_upload_cover_image(p.id, image_path)
 
 token = secret.token
 
 if token:
     sp = spotipy.Spotify(auth=token)
+    csp = secret.csp
     #results = sp.current_user_saved_tracks()
     #results = sp.recommendation_genre_seeds()
     #results = sp.current_user_top_tracks(limit=100)
@@ -124,25 +129,40 @@ if token:
     playlists = list()
     pattern = '([\w:/.]+/playlist/)(?P<id>[\w]+)(\?[\w=]+)*'
     prog = re.compile(pattern)
+    src_found = False
+    dest_found = False
     
     while taking_input:
-        pl = input("Link to playlist, 'done' otherwise: ")
-        if pl == 'done':
+        if not src_found:
+            src = input("Playlist to copy: ")
+        else:
+            dest = input("Playlist to replace: ")
+        if src_found and dest_found:
             if playlists:
                 taking_input = False
             else:
                 print('Playlist list cannot be empty')
         else:
-            if prog.match(pl):
-                playlists.append(pl)
+            if not src_found and prog.match(src):
+                src_found = True
+            elif not dest_found and prog.match(dest):
+                break
             else:
                 print('Invalid URL - Could not find playlist ID')
-            
-    for link in playlists:
+     
+    #folding laundry
+    src_pl = sp.playlist(prog.match(src).group('id'))   
+    spl = Playlist(name=src_pl['name'], p_id=src_pl['id'])
+    
+    dest_pl = sp.playlist(prog.match(dest).group('id'))
+    dpl = Playlist(name=dest_pl['name'], p_id=dest_pl['id'])
+ 
+    playlist_destruct(spl, dpl)
+    #for link in playlists:
         #print(prog.match(link).group('id'))
-        p = sp.playlist(prog.match(link).group('id'))
-        plist = Playlist(name=p['name'], p_id=p['id'])
-        playlist_destruct(plist)
+    #    p = sp.playlist(prog.match(link).group('id'))
+    #    plist = Playlist(name=p['name'], p_id=p['id'])
+        #playlist_destruct(plist)
    
      
  

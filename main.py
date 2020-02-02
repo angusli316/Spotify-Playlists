d4e4216647db054cb758ba3a859dc55da9e694a4
prestontai <prestonttt@yahoo.com>
2020-02-01 17:25:20 -0800

    stats

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def history():
     for track in results['items']:
         print_info(track['track'])
 
+song_freq = dict()
+
 class Track:
     def __init__(self, name, artist):
         self.name = name
@@ -28,13 +30,18 @@ class Playlist:
         self.tracks = []
         for t in tracks['items']:
             #print(t['track']['artists'][0]['name'])
-            self.tracks.append(Track(name=t['track']['name'], artist=t['track']['artists'][0]['name']))
+            track_object = t['track']['name'], t['track']['artists'][0]['name']
+            if track_object in song_freq:
+                song_freq[track_object] += 1
+            else:
+                song_freq[track_object] = 1
+            self.tracks.append(Track(name=track_object[0], artist=track_object[1]))
 
 
 def playlist_info():
     results = sp.current_user_playlists()
     playlist_list = []
-    
+  
     for p in results['items']:
         new_playlist = Playlist(name=p['name'], p_id=p['id'])
         playlist_list.append(new_playlist)
@@ -42,12 +49,15 @@ def playlist_info():
         print(new_playlist.tracks) 
         print()
 
+    for track in sorted(song_freq.items(), key= lambda t:t[1], reverse=True):
+        print(track)
+
 token = secret.token
 
 if token:
     sp = spotipy.Spotify(auth=token)
     #results = sp.current_user_saved_tracks()
-    results = sp.recommendation_genre_seeds()
+    #results = sp.recommendation_genre_seeds()
     #results = sp.current_user_top_tracks(limit=100)
     
     #history()
